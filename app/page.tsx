6e1783fa6330137df1e7960c45a15bbe2d903426
MihailAm <amosov.mihail2280@gmail.com>
2023-11-12 19:36:14 +0300

    Основная страничка

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
 import { Metadata } from 'next';
-import {P, Button, Htag } from '../components';
+import {Tag, P, Button, Htag } from '../components';
 import cn from 'classnames';
 
 
 
 export default function Home():JSX.Element {
   return (
-    <div>
+    <>
       <Htag tag='h1'>Заголовок</Htag>
       <Button appearance='primary' arrow='right'>Записаться</Button>
       <Button appearance='ghost' arrow='down'>Оставить отзыв</Button>
       <P size='l'>Большой</P>
       <P>Средний</P>
       <P size='s'>Маленький</P>
-    </div>
+      <Tag size='s'>Ghost</Tag>
+      <Tag size='m' color='red'>Red</Tag>
+      <Tag size='s' color='green'>Green</Tag>
+      <Tag color='primary'>Green</Tag>
+    </>
   
   )
 }
